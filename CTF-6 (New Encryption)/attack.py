e4b93d1a42dd1a4f2c4b78b3c0b7809437bbaab6
aliaagheisX <aliaagheis@gmail.com>
2024-04-13 23:00:15 +0200

    clean code

---
+++ CTF-6 (New Encryption)/attack.py
@@ -0,0 +1,34 @@
+import string
+from decrypt import decrypt
+
+
+def attack_one_char_key(cipher: str) -> list:
+    """
+    Attempt to decrypt a cipher text using all possible single character keys.
+
+    Args:
+    - cipher (str): The cipher text to decrypt.
+
+    Returns:
+    - list: A list of possible decrypted messages.
+    """
+    possible_msgs = []
+    
+    for i in string.ascii_lowercase[:16]: # iterate only on first 16 characters (as it only needed)
+        msg = decrypt(cipher, i) # attempt decryption using this character
+        
+        # check if attempt is valid by checking if output msg is ascii
+        if msg.isascii():
+            possible_msgs.append(msg)
+        
+    return possible_msgs
+        
+if __name__ == "__main__":
+    with open("./cipher.txt", "r") as cipher_file:
+        cipher = cipher_file.read()
+        
+        possible_msgs = attack_one_char_key(cipher)
+
+        with open("./possible_results.txt", "w") as out_file:
+            out_file.write("\n".join(possible_msgs))
+        
