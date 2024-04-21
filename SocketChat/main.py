c6532f11b6029121d32fc1faca68943744943c3e
abdallahahmedali <abdallahahmedali@outlook.com>
2024-04-21 18:50:52 +0200

    Server-Client: Python Scripts

--- SocketChat/main.py
+++ SocketChat/main.py
@@ -1,6 +1,8 @@
 from DiffieHellman import DiffieHellman
 from Elgamal import Elgamal
 from Hashing import Hashing
+import tkinter as tk
+
 
 
 def main():
@@ -20,9 +22,24 @@ def main():
     # Proving that when a different public key is sent the signature verifying failed
     m = hashing.hash(message)
     s1, s2 = elgamal.sign_message(m)
-    print(elgamal.verify_signature(m, s1, s2, elgamal2.get_public_key()))
+    print(elgamal.verify_signature(m, s1, s2, elgamal.get_public_key()))
 
+def open_second_window():
+    second_window = tk.Toplevel(root)
+    second_window.title("Second Window")
+    label = tk.Label(second_window, text="This is the second window")
+    label.pack()
 
+root = tk.Tk()
+root.title("Main Window")
+
+label = tk.Label(root, text="This is the main window")
+label.pack()
+
+button = tk.Button(root, text="Open Second Window", command=open_second_window)
+button.pack()
+
+root.mainloop()
 # If the script is executed directly (not imported as a module)
 if __name__ == "__main__":
     main()
